a326e079886820a9f8d41f28cb936b945234e76c
Justin Coker <jcoker@JCoker-Air.local>
2019-11-25 11:55:51 -0600

    Added wrapper functions for delete and all.

--- lib/collabiq/repo.ex
+++ lib/collabiq/repo.ex
@@ -5,10 +5,48 @@ defmodule Collabiq.Repo do
 
   alias Collabiq.{Error, UUID}
 
-  def put(changeset) do
+  def full(query, opts \\ []) do
+    id = Keyword.get(opts, :id, :base_id)
+
+    case __MODULE__.all(query) do
+      [] ->
+        []
+
+      list ->
+        if id == :base_id do
+          Enum.map(list, &replace_id_in_struct/1)
+        else
+          list
+        end
+    end
+  end
+
+  def purge(changeset, opts \\ []) do
+    id = Keyword.get(opts, :id, :base_id)
+
+    case __MODULE__.delete(changeset) do
+      {:ok, struct} ->
+        if id == :base_id do
+          {:ok, replace_id_in_struct(struct)}
+        else
+          {:ok, struct}
+        end
+
+      {:error, change} ->
+        Error.message(change)
+    end
+  end
+
+  def put(changeset, opts \\ []) do
+    id = Keyword.get(opts, :id, :base_id)
+
     case __MODULE__.insert_or_update(changeset) do
       {:ok, result} ->
-        {:ok, replace_id_in_struct(result)}
+        if id == :base_id do
+          {:ok, replace_id_in_struct(result)}
+        else
+          {:ok, result}
+        end
 
       {:error, change} ->
         Error.message(change)
@@ -18,7 +56,7 @@ defmodule Collabiq.Repo do
   def replace_id_in_struct(struct) do
     with {:ok, base_id} <- UUID.string_to_base(struct.id),
          result <- Map.put(struct, :id, base_id) do
-        result
+      result
     else
       error ->
         error
@@ -61,7 +99,7 @@ defmodule Collabiq.Repo do
       %_{} ->
         {:ok, data}
 
-      [ %_{} | _ ] ->
+      [%_{} | _] ->
         {:ok, data}
 
       _ ->

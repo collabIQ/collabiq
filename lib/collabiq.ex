3aa7c134bc45a612f4f956a556982fc8d180e310
Justin Coker <jcoker@JCoker-Air.local>
2019-11-26 14:50:29 -0600

    Created delegate functions to Collabiq

--- lib/collabiq.ex
+++ lib/collabiq.ex
@@ -1,9 +1,34 @@
 defmodule Collabiq do
-  @moduledoc """
-  Collabiq keeps the contexts that define your domain
-  and business logic.
+  @moduledoc false
+  alias Collabiq.{Directory, Proxy, Site}
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  ### Directory Functions ###
+  defdelegate create_directory(attrs, sess, opts \\ []), to: Directory, as: :create
+  defdelegate delete_directory(id, sess, opts \\ []), to: Directory, as: :delete
+  defdelegate disable_directory(id, sess, opts \\ []), to: Directory, as: :disable
+  defdelegate enable_directory(id, sess, opts \\ []), to: Directory, as: :enable
+  defdelegate get_directory(id, sess, opts \\ []), to: Directory, as: :get
+  defdelegate list_directories(attrs, sess, opts \\ []), to: Directory, as: :list
+  defdelegate purge_directory(id, sess, opts \\ []), to: Directory, as: :purge
+  defdelegate update_directory(id, sess, opts \\ []), to: Directory, as: :update
+
+  ### Proxy Functions ###
+  defdelegate create_proxy(attrs, sess, opts \\ []), to: Proxy, as: :create
+  defdelegate delete_proxy(id, sess, opts \\ []), to: Proxy, as: :delete
+  defdelegate disable_proxy(id, sess, opts \\ []), to: Proxy, as: :disable
+  defdelegate enable_proxy(id, sess, opts \\ []), to: Proxy, as: :enable
+  defdelegate get_proxy(id, sess, opts \\ []), to: Proxy, as: :get
+  defdelegate list_proxies(attrs, sess, opts \\ []), to: Proxy, as: :list
+  defdelegate purge_proxy(id, sess, opts \\ []), to: Proxy, as: :purge
+  defdelegate update_proxy(id, sess, opts \\ []), to: Proxy, as: :update
+
+  ### Site Functions ###
+  defdelegate create_site(attrs, sess, opts \\ []), to: Site, as: :create
+  defdelegate delete_site(id, sess, opts \\ []), to: Site, as: :delete
+  defdelegate disable_site(id, sess, opts \\ []), to: Site, as: :disable
+  defdelegate enable_site(id, sess, opts \\ []), to: Site, as: :enable
+  defdelegate get_site(id, sess, opts \\ []), to: Site, as: :get
+  defdelegate list_sites(attrs, sess, opts \\ []), to: Site, as: :list
+  defdelegate purge_site(id, sess, opts \\ []), to: Site, as: :purge
+  defdelegate update_site(id, sess, opts \\ []), to: Site, as: :update
 end
